08d51e36287791d48a3f1229952e02d575f55414
CptPie <23438606+CptPie@users.noreply.github.com>
2022-12-04 05:40:43 +0100

    Prepared days 5 to 10

---
+++ 2022/day06/solve_test.go
@@ -0,0 +1,85 @@
+package day06
+
+import "testing"
+
+func Test_solvePart1(t *testing.T) {
+	type args struct {
+		fileContents []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "Part1 - should return",
+			args: args{[]string{
+				"",
+			}},
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name: "Part1 - should return error",
+			args: args{[]string{
+				"",
+			}},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := solvePart1(tt.args.fileContents)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("solvePart1() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("solvePart1() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_solvePart2(t *testing.T) {
+	type args struct {
+		fileContents []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "Part2 - should return ",
+			args: args{[]string{
+				"",
+			}},
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name: "Part2 - should return error",
+			args: args{[]string{
+				"",
+			}},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := solvePart2(tt.args.fileContents)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("solvePart2() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("solvePart2() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
