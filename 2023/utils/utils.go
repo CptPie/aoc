2acc2b4d45dbd345eb095f8af431420d9ed097d1
CptPie <23438606+CptPie@users.noreply.github.com>
2023-12-08 00:31:36 +0100

    Added auto bootstrap for new days
    Had to "creatively" borrow some code from stackoverflow since there is
    no native way to copy a folder to a new location...
    Source: https://stackoverflow.com/a/56314145

--- 2023/utils/utils.go
+++ 2023/utils/utils.go
@@ -11,8 +11,10 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -181,12 +183,9 @@ func requestDesc(config Config, year int, day int) error {
 
 	foldername := fmt.Sprintf("day%02d", day)
 
-	_, err = os.Stat(foldername)
-	if os.IsNotExist(err) {
-		err = os.Mkdir(foldername, os.ModePerm)
-		if err != nil {
-			return err
-		}
+	ensureFolder(foldername)
+	if err != nil {
+		return err
 	}
 
 	file, err := os.Create(foldername + "/description.md")
@@ -259,12 +258,9 @@ func requestInput(config Config, year int, day int) error {
 
 	foldername := fmt.Sprintf("day%02d", day)
 
-	_, err = os.Stat(foldername)
-	if os.IsNotExist(err) {
-		err = os.Mkdir(foldername, os.ModePerm)
-		if err != nil {
-			return err
-		}
+	err = ensureFolder(foldername)
+	if err != nil {
+		return err
 	}
 
 	file, err := os.Create(foldername + "/input")
@@ -281,4 +277,123 @@ func requestInput(config Config, year int, day int) error {
 		return err
 	}
 	return nil
+}
+
+func ensureFolder(foldername string) error {
+	_, err := os.Stat(foldername)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(foldername, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		CopyDirectory("templateFolder", foldername)
+	}
+	return nil
+}
+
+func CopyDirectory(scrDir, dest string) error {
+	entries, err := os.ReadDir(scrDir)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		sourcePath := filepath.Join(scrDir, entry.Name())
+		destPath := filepath.Join(dest, entry.Name())
+
+		fileInfo, err := os.Stat(sourcePath)
+		if err != nil {
+			return err
+		}
+
+		stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+		if !ok {
+			return fmt.Errorf("failed to get raw syscall.Stat_t data for '%s'", sourcePath)
+		}
+
+		switch fileInfo.Mode() & os.ModeType {
+		case os.ModeDir:
+			if err := CreateIfNotExists(destPath, 0755); err != nil {
+				return err
+			}
+			if err := CopyDirectory(sourcePath, destPath); err != nil {
+				return err
+			}
+		case os.ModeSymlink:
+			if err := CopySymLink(sourcePath, destPath); err != nil {
+				return err
+			}
+		default:
+			if err := Copy(sourcePath, destPath); err != nil {
+				return err
+			}
+		}
+
+		if err := os.Lchown(destPath, int(stat.Uid), int(stat.Gid)); err != nil {
+			return err
+		}
+
+		fInfo, err := entry.Info()
+		if err != nil {
+			return err
+		}
+
+		isSymlink := fInfo.Mode()&os.ModeSymlink != 0
+		if !isSymlink {
+			if err := os.Chmod(destPath, fInfo.Mode()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func Copy(srcFile, dstFile string) error {
+	out, err := os.Create(dstFile)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	in, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Exists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func CreateIfNotExists(dir string, perm os.FileMode) error {
+	if Exists(dir) {
+		return nil
+	}
+
+	if err := os.MkdirAll(dir, perm); err != nil {
+		return fmt.Errorf("failed to create directory: '%s', error: '%s'", dir, err.Error())
+	}
+
+	return nil
+}
+
+func CopySymLink(source, dest string) error {
+	link, err := os.Readlink(source)
+	if err != nil {
+		return err
+	}
+	return os.Symlink(link, dest)
 }
